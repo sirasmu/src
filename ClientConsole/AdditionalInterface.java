aac01475475b6fcc5c55197fb7e3c319c2f12b87
vpetermio <vpetermio@yahoo.com>
2016-11-07 16:33:05 +0100

    additional interface

---
+++ ClientConsole/AdditionalInterface.java
@@ -0,0 +1,8 @@
+package ClientConsole;
+	import java.rmi.Remote;
+	import java.rmi.RemoteException;
+
+	public interface AdditionalInterface extends Remote {
+	    public int Add(int a, int b) throws RemoteException;
+	}
+
