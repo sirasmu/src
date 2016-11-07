abb9a9c63ab9ea5fe7b07d5668642d797ca0b6a5
Legio17 <rokuro07@gmail.com>
2016-11-07 10:13:18 +0100

    Reg and adapter update

--- package1/Adapted_txt.java
+++ package1/Adapted_txt.java
@@ -1,5 +1,27 @@
 package package1;
 
-public class Adapted_txt {
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
 
+public class Adapted_txt {
+	
+	private static String[][] list_address;
+
+	public Adapted_txt(String path) {
+		try {
+			readFromFile(path);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	
+	}
+	
+	public String[][] readFromFile(String path){
+		
+		
+		return null;
+		
+	}
 }
