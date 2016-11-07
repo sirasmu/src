c420cef56bd14d1ff83ad6983ade062d02c168b2
S. Rasmussen <siras@outlook.com>
2016-11-07 11:12:26 +0100

    TestReservedFileAdapter reserved example bin

--- WIP/data/TestReservedFileAdapter.java
+++ WIP/data/TestReservedFileAdapter.java
@@ -29,17 +29,11 @@ public class TestReservedFileAdapter
       reservedList = rfa.getAllReserveds();
       System.out.println(reservedList);
 
-      /*
-       * Reserved book1= new Reserved(list.get(2), resNo, new TheTime(1,3,2016),new
-       * TheTime(5,3,2016), "horsens","horsens","ana","chifor",200,1000);
-       * //book1.setReserved("abc","def"); //book1.setReturn(new
-       * TheTime(19,4,2016),200, "good", 1000); rentList.addReserved(book1); resNo =
-       * rentList.size(); Reserved book2= new Reserved(list.get(3), resNo, new
-       * TheTime(16,3,2016),new TheTime(17,3,2016),
-       * "horsens","aarhus","maria","chifor",100,1000); rentList.addReserved(book2);
-       * System.out.println("here:\n" + rentList); rfa.saveReserveds(rentList);
-       * rentList = rfa.getAllReserveds(); VehicleList v = rfa.timeIsAvailable(list,
-       * new TheTime(2,3,2016),new TheTime(9,3,2016)); System.out.println(v);
-       */
+      
+       Reserved book1= new Reserved(list.get(3), 1, new TheTime(16,3,2016),new TheTime(17,3,2016),  "horsens","aarhus","maria","chifor",100,1000); 
+       reservedList.add(book1);
+       System.out.println("here:\n" + reservedList); rfa.saveReserveds(reservedList);
+       reservedList = rfa.getAllReserveds(); 
+       
    }
 }
