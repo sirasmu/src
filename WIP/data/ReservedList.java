c9f08037be898b707d630805099a3de5b559b701
S. Rasmussen <siras@outlook.com>
2016-11-07 15:41:28 +0100

    ReservedList documention

--- WIP/data/ReservedList.java
+++ WIP/data/ReservedList.java
@@ -7,34 +7,50 @@ import WIP.data.utility.ListADT;
 
 public class ReservedList implements Serializable {
 
-	private ListADT<Reserved> list;
+	private ListADT<Reserved> reservedList;
 
 	/**
 	 * The list for Reserved objects is initialized
 	 */
 	public ReservedList() {
-		list = new ArrayList<>();
+		reservedList = new ArrayList<>();
 	}
 
+	/**
+	 * @return a list of all reservations saved in a binary file stored in the server
+	 */
 	public ListADT<Reserved> getAll() {
-		return list;
+		return reservedList;
 	}
-
+	
+	/**
+	 * @param index of the Reserved object
+	 * @return Reserved object
+	 */
 	public Reserved get(int index) {
-		return list.get(index);
+		return reservedList.get(index);
 	}
 
+	/**
+	 * @param reserved is the new object to add in the end of the list
+	 */
 	public void add(Reserved reserved) {
-		list.add(reserved);
+		reservedList.add(reserved);
 	}
-
+	
+	/**
+	 * @return size of list
+	 */
 	public int size() {
-		return list.size();
+		return reservedList.size();
 	}
-
+	
+	/**
+	 * Every object in the list is printed out in order
+	 */
 	public String toString() {
 		StringBuilder str = new StringBuilder("****CURRENT RESERVATIONS****\n");
-		for (int i = 0; i < list.size(); i++) {
+		for (int i = 0; i < reservedList.size(); i++) {
 			str.append(get(i) + "\n");
 		}
 		str.append("END");
