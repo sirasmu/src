9628dce53cb1705f70a93ccc7f641801f5372f12
vpetermio <vpetermio@yahoo.com>
2016-11-09 15:15:13 +0100

    implemented serializable

--- ClientConsole/Client.java
+++ ClientConsole/Client.java
@@ -3,15 +3,17 @@ package ClientConsole;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.Serializable;
 import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
+
 import WIP.data.Reserved;
 import WIP.data.ReservedList;
 import WIP.data.utility.TheTime;
 
-public class Client {
+public class Client implements Serializable{
 
 	private String remoteHostName;
 	private int remotePort;
