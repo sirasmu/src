1cbb18d13ab7ea412a22485694077cd9624647e8
S. Rasmussen <siras@outlook.com>
2016-11-09 10:36:17 +0100

    Fixed controller sync problem

--- ClientConsole/Controller.java
+++ ClientConsole/Controller.java
@@ -2,28 +2,42 @@ package ClientConsole;
 
 import java.nio.channels.IllegalSelectorException;
 import java.rmi.RemoteException;
-
-
+import WIP.data.ReservedList;
+import WIP.data.Reserved;
+import java.util.Iterator;
+import WIP.data.Reserved;
 import WIP.data.ReservedList;
 
-public class Controller{
-private InterfaceModel model;
-public Controller(InterfaceModel model)
-{
-	this.model=model;
-}
+public class Controller {
+	private InterfaceModel model;
 
-public ReservedList getAllReservation()
-{
-	try {
-		return model.getAll();
-	} catch (RemoteException e) {
-		e.printStackTrace();
+	public Controller(InterfaceModel model) {
+		this.model = model;
 	}
-	throw new IllegalSelectorException();
-}
 
-
+	public ReservedList getAll() {
+		try {
+			return model.getAll();
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
+		throw new IllegalSelectorException();
+	}
 	
+	public Reserved getReservation(int resNo){
+		
+		Iterator<Reserved> iterator = getAll().iterator();
+		while(iterator.hasNext()){
+			Reserved r = iterator.next();
+			if(r.getResNo() == resNo){
+				return r;
+			}
+		}
+		throw new IllegalArgumentException("No Reserved with that reservation number was found");
+	}
+	
+	public Reserved removeReservation(int resNo){
+		Reserved r = getReservation(resNo);
+		return getAll().remove(r);
+	}
 }
-
