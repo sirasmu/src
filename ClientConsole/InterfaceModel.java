eeced8b9320088685cbc6ed571cbd2563b475704
S. Rasmussen <siras@outlook.com>
2016-11-09 10:06:36 +0100

    Import

--- ClientConsole/InterfaceModel.java
+++ ClientConsole/InterfaceModel.java
@@ -1,8 +1,8 @@
 package ClientConsole;
 	import java.rmi.Remote;
 import java.rmi.RemoteException;
-
-import WIP.data.*;
+import WIP.data.ReservedList;
+import WIP.data.Reserved;
 
 	public interface InterfaceModel extends Remote {
 	     ReservedList getAll() throws RemoteException;
