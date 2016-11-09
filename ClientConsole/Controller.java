8248e17c228a6a8bcbe209e876d62bff011a4205
anaiuliachifor <anaiuliachifor@gmail.com>
2016-11-09 10:26:11 +0100

    added the update method for the observer

--- ClientConsole/Controller.java
+++ ClientConsole/Controller.java
@@ -2,10 +2,12 @@ package ClientConsole;
 
 import java.nio.channels.IllegalSelectorException;
 import java.rmi.RemoteException;
+import java.util.Observable;
+import java.util.Observer;
 
 import WIP.data.ReservedList;
 
-public class Controller {
+public class Controller implements Observer {
 private InterfaceModel model;
 public Controller(InterfaceModel model)
 {
@@ -21,4 +23,27 @@ public ReservedList getAllReservation()
 	}
 	throw new IllegalSelectorException();
 }
+
+@Override
+public void update(Observable o, Object arg) {
+	String message=arg.toString();
+	String reply="";
+	String[] split = message.split(":");
+	
+	if (split.length != 2) {
+		reply = "Wrong command";
+	}
+	else if(split[0].equalsIgnoreCase("Delete")){
+		reply="A car was deleted";
+	}
+	else if(split[0].equalsIgnoreCase("Book")){
+		reply="A car was booked";
+	}
+	
+	System.out.println("Something changed: "+reply);
+	
+	
 }
+	
+}
+
