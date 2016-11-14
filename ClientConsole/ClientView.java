94ce691e00ce1734544edcbac514cec268f5776a
Legio17 <rokuro07@gmail.com>
2016-11-14 09:57:57 +0100

    addServerMenu

--- ClientConsole/ClientView.java
+++ ClientConsole/ClientView.java
@@ -3,6 +3,7 @@ package ClientConsole;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 import WIP.data.Reserved;
 import WIP.data.ReservedList;
@@ -12,9 +13,24 @@ public class ClientView {
 
 	private ClientController controller;
 	private BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+	private String choosenServerAddress; //address client choose for establishing server connection
 
 	public void addController(ClientController controller) {
 		this.controller = controller;
+	}
+	
+	/**
+	 * Displays the menu for action selection
+	 */
+	public void ServerMenu(ArrayList<String[]> serverList) {
+
+		System.out.println(" Select server: ");
+		int serverNumber = 1;
+		
+		for (int i = 0; i < serverList.size(); i++){
+			System.out.println(serverNumber+". "+serverList.get(serverNumber-1)[0]);
+			serverNumber ++;
+		}
 	}
 
 	/**
