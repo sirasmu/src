6b6a006b2256072bc814b56da437df05d6adb66d
vpetermio <vpetermio@yahoo.com>
2016-11-17 13:10:47 +0100

    edited sysout

--- ClientConsole/Client.java
+++ ClientConsole/Client.java
@@ -23,22 +23,23 @@ public class Client {
 	}
 
 	private void connectToServer() {
-		String connectLocation = "//" + remoteHostName + ":" + remotePortRegistry + "/Connect";
+		String connectLocation = "//" + remoteHostName + ":" + remotePortRegistry + "/DirectoryServer";
 		String in = null;
 		try {
-			System.out.println("Connecting to client at : " + connectLocation);
+			System.out.println("Connecting to the directory server at: " + connectLocation);
 
 			InterfaceNameAndIP namemodel = (InterfaceNameAndIP) Naming.lookup(connectLocation);
 			String[] list = namemodel.getAllNames();
 
-			System.out.println("The list: ");
-
+			System.out.println("\nSelect a main server from the list: ");
+			
 			StringBuffer result = new StringBuffer();
 			for (int i = 0; i < list.length; i++) {
 				result.append(list[i] + "\n");
 			}
 			String mynewstring = result.toString();
 			System.out.println(mynewstring);
+			System.out.println("Type the name of the chosen server: ");
 			try {
 				in = reader.readLine();
 			} catch (IOException e) {
@@ -46,8 +47,8 @@ public class Client {
 			}
 			String remoteHostName2 = namemodel.getIP(in);
 
-			String connectLocation2 = "//" + remoteHostName2 + ":" + remotePortServer + "/Connect2";
-			System.out.println("Connecting to client at : " + connectLocation2);
+			String connectLocation2 = "//" + remoteHostName2 + ":" + remotePortServer + "/MainServer";
+			System.out.println("Connecting to the main server at: " + connectLocation2);
 			InterfaceModel model = (InterfaceModel) Naming.lookup(connectLocation2);
 			new ClientController(model, new ClientView());
 		} catch (MalformedURLException e1) {
@@ -60,7 +61,7 @@ public class Client {
 	}
 
 	public static void main(String[] args) {
-		new Client("localhost", 1099, 1098);
+		new Client("10.52.237.108", 1099, 1098);
 		// ServerMenu();
 		new Client("localhost", 1099, 1098);
 	}
